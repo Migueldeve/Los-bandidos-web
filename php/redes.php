296211fd9af5945d9aed2edf22304a75d1843f06
MiguelDeveloper12 <momentosconhotwheels@gmail.com>
2022-08-10 00:12:18 -0500

    se implemento grid ajustable y se optimizo el Header etc

--- php/redes.php
+++ php/redes.php
@@ -4,19 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/index.css">
+    <title>losBandidos</title>
+
 </head>
 <body>
-    
-    <ul class="menu">
-         /*header*/
-		 <li><a href="index.php">Inicio</a></li>
-		 <li><a href="clips-virales.php">Clips virales</a></li>
-		 <li><a href="biografias.php">Biografias</a></li>
-		 <li><a href="redes.php">Redes</a></li>
-         <li><a href="">Contacto</a></li>
-	</ul>
-
+    <?php require('./resources/header.html') ?>
 </body>
 </html>
